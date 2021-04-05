59feb2c758870999be5fe5693bfe94ae23ea466d
suiunbek1 <mirbekov.suiunbek.9.02@gmail.com>
2021-04-05 17:25:30 +0300

    first upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+a=('hello world')
+alert(a)
+
+
+// const swiperReviev = new Swiper('#Understanding .swiper-container', {
+//     slidesPerView:3,
+//     watchOverflow: true,
+//     pagination: {
+//         el: '.swiper-pagination',
+//         type: 'fraction',
+//     },
+//     autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false
+//     },
+//     navigation: {
+//         nextEl: '.button-next',
+//         prevEl: '.button-prev',
+//     },
+// })
+
